bb0c8f724e947264dfac21eb27b60f2368da746c
Dylan Hathaway <hathawayd@rsd407.org>
2021-05-24 08:03:04 -0700

    more work

--- Base project/MyGame/MyGame/GameScene.cpp
+++ Base project/MyGame/MyGame/GameScene.cpp
@@ -1,10 +1,16 @@
 #include "GameScene.h"
 #include"ship.h"
 #include "meteorspawners.h"
+#include "score.h"
 
 int GameScene::getScore()
 {
 	return score_;
+}
+
+void GameScene::increasesScore()
+{
+	++score_;
 }
 
 
@@ -15,4 +21,7 @@ GameScene::GameScene()
 
 	MeteorSpawnerPtr MeteorSpawner = std::make_shared<MeteorSpawner>();
 	addGameObject(MeteorSpawner);
+
+	ScorePtr score = std::make_shared<Score>(sf::Vector2f(10.0f, 10.0f));
+	addGameObject(score);
 }
